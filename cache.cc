c496f53af87bac60319f3ba23fa034c6bdb09ecf
inhoinno <mearrong123@gmail.com>
2021-12-23 19:01:12 +0900

    N_Way_Set_cache::store_tag:Now store tag includes eviction algorithm

--- cache.cc
+++ cache.cc
@@ -88,7 +88,7 @@ void Direct_mapped_cache::placement_policy(char operation_type, std::string phys
             
             logger->cold_miss();
             store_tag(physical_address);
-            set_valid_bit(get_index(physical_address));
+            semearrt_valid_bit(get_index(physical_address));
 
             if (!write_flag) {  
                 //if ld instruction coldmiss then goto mem and fetch data
@@ -190,7 +190,8 @@ void Direct_mapped_cache::set_dirty_bit(uint32_t cache_index){
 }
 
 void N_Way_Set_cache::placement_policy(char operation_type, std::string physical_address){
-    bool write_flag=false;     
+    bool write_flag=false;   
+    uint32_t i = 0;  
     cout<< "N_way_set_cache::placement_policy : operation:"<< operation_type<<" address:" << physical_address <<endl;
     if(operation_type == 'l'){
         logger->load();
@@ -217,10 +218,10 @@ void N_Way_Set_cache::placement_policy(char operation_type, std::string physical
         //      then call eviction algorithm so make cache has a room
         //  place physical address block to cache. tag updated.
         
-        if(!is_valid(physical_address)){
-            
-            logger->cold_miss();
-            store_tag(physical_address);
+
+        if(!is_valid(physical_address)){    //if cold miss
+            logger->cold_miss(); 
+            store_tag(physical_address);    //store_tag
             set_valid_bit(physical_address);
 
             if (!write_flag) {  
@@ -329,6 +330,7 @@ uint32_t N_Way_Set_cache::get_way(std::string physical_address){
     bitset<32> * match;
     bitset<32> mask(0);
     uint32_t i = 0;
+    //uint32_t j = 0;
     mask = mask.flip();
     mask = mask << __valid_bit_offset;
     mask = mask.flip(); //tag, index, offset
@@ -337,11 +339,19 @@ uint32_t N_Way_Set_cache::get_way(std::string physical_address){
         match = new bitset<32>(tagarray[i][pidx.to_ulong()]->to_ulong() & mask.to_ulong());
         if ( match->to_ulong() == ptag.to_ulong()) break;
     }
+    if (i==_blocks) return 0;
     return i;
 }
 
 bool N_Way_Set_cache::is_valid(std::string physical_address){
-    return _is_valid(get_way(physical_address), get_index(physical_address));
+    uint32_t idx = get_index(physical_address);
+    uint32_t i =0
+    for (i= 0; i<_blocks; i++){
+        if(!_is_valid(i, idx))   //true if any entry is invalid
+            break;
+    }
+    //return false if any one entry is invalid 
+    return (i!=_blocks)? false : true;  //i:-1
 }
 bool N_Way_Set_cache::is_dirty(std::string physical_address){
         return _is_dirty(get_way(physical_address), get_index(physical_address));
@@ -354,11 +364,12 @@ void N_Way_Set_cache::set_dirty_bit(std::string physical_address){
 }
 
 bool N_Way_Set_cache::_is_valid(uint32_t way, uint32_t cache_index){
-    bitset<32> mask(1);
-    bitset<32> val(tagarray[way].at(cache_index)->to_ulong());
-    mask = mask << __valid_bit_offset;
-    mask = mask & val;
-    return (mask.to_ulong() == 1) ? true : false ;
+    //bitset<32> mask(1);     //1111  1 11 11
+    bitset<32> val(tagarray[way].at(cache_index)->to_ulong()); // 0 00 00
+    //mask = mask << __valid_bit_offset; //1 00 00
+    //mask = mask & val; // 1 00 00 
+    //return (mask.to_ulong() == 0) ? false : true ;
+    return (val[_valid_bit_offset] == 1)? true: false;
 }
 bool N_Way_Set_cache::_is_dirty(uint32_t way, uint32_t cache_index){
     bitset<32> mask(1);
@@ -375,18 +386,50 @@ void N_Way_Set_cache::_set_dirty_bit(uint32_t way, uint32_t cache_index){
 
 }
 void N_Way_Set_cache::store_tag(std::string physical_address){
-    cout<<physical_address<<endl;
+    cout<<"N_Way_Set_cache::store_tag:"<<physical_address<<endl;
     uint32_t tag = get_tag(physical_address);
-    tagarray[][get_index(physical_address)] = new bitset<32>(tag);  //N-Way store tag
+    uint32_t idx = get_index(physical_adderss);
+    uint32_t way = 0;
+
+    /* 
+    Is this function necessary?
+    */
+    for (way = 0; way < _blocks; way++)
+        if(!_is_valid(way,idx))
+            break;
+
+    if(way != _blocks)
+        _store_tag(way,idx);
+    else if(way == _blocks){
+        way = eviction_algorithm();
+        _store_tag(way,idx);
+    }
+    return;
+    }
+void N_Way_Set_cache::_store_tag(uint32_t way, uint32_t cache_index){
+    cout<<"N_Way_Set_cache::store_tag:"<<physical_address<<endl;
+    uint32_t tag = get_tag(physical_address);
+    uint32_t idx = cache_index;
+    /* 
+    If store tag only call by Cache miss, 
+    since get_way return exact way of pm,
+    then 
+    */
+    tagarray[way][idx] = new bitset<32>(tag);  //N-Way store tag
     return;
     }
 
 int N_Way_Set_cache::cache_lookup_algorithm(std::string physical_address){
+    uint32_t i =0 ;
+    uint32_t ptag = get_tag(physical_address);
+    uint32_t pidx = get_index(physical_adderss);
 
     cout<<"N_Way::cache_lookup_algorithm"<<physical_address<<endl;
-	if(get_tag(physical_address) == cache_lookup(physical_address) && is_valid(physical_address)){
-        	//then HIT
-        return 1;
+	for(int i=0; i<_blocks; i++){
+        if(ptag == _cache_lookup(i,pidx) && _is_valid(i,pidx)){
+                //then HIT
+            return 1;
+        }
     }
     return 0;
 }
